6b16457b58630f7d740ad6c7679d49b9b8db47bc
Dmitry Ponyatov <dponyatov@gmail.com>
2019-10-19 18:50:06 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- wasm.py
+++ wasm.py
@@ -3,6 +3,8 @@
 const = 'const'
 mul   = 'mul'
 add   = 'add'
+load  = 'load'
+stor  = 'stor'
 
 class Stack:
     def __init__(self):
@@ -12,24 +14,28 @@ class Stack:
     def pop(self):
         return self.items.pop()
     def __repr__(self):
-        return '[%s]' % self.items
+        return ' stack:%s' % self.items
 
 class Machine(Stack):
     def __init__(self,code):
         Stack.__init__(self)
         self.code = code
+        self.mem  = {}
+    def __repr__(self):
+        return Stack.__repr__(self) + ' mem:%s'%self.mem
     def add(self):
         R = self.pop() ; L = self.pop() ; self.push(L + R)
     def mul(self):
         R = self.pop() ; L = self.pop() ; self.push(L * R)
     def eval(self):
-        for cmd in self.code:
-            op = cmd[0] ; args = cmd[1:]
-            print op,args
+        for op,args in ([i[0],i[1:]] for i in self.code):
+            print 'op:',op,'args:',args,self
             {
                 const:lambda: self.push(args[0]),
                 add  :lambda: self.add(),
-                mul  :lambda: self.mul()
+                mul  :lambda: self.mul(),
+                load :lambda: self.load(),
+                stor :lambda: self.stor(),
             }[op]()
         return self
         
